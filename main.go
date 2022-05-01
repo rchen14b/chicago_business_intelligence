c682ca24408f9cccc6ea40345aecf1471c9d3b95
Ruiyang Chen <rchen29@hawk.iit.edu>
2022-05-01 15:31:07 -0500

    fix database host address

--- main.go
+++ main.go
@@ -129,7 +129,7 @@ func main() {
 	//db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=host.docker.internal sslmode=disable port = 5433"
 	//Option 4
 	//Database application running on Google Cloud Platform.
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intelligence9:us-central1:mypostgres sslmode=disable port = 5432"
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intelligence9:us-central1:mypostgres1 sslmode=disable port = 5432"
 
 	db, err := sql.Open("postgres", db_connection)
 	if err != nil {
